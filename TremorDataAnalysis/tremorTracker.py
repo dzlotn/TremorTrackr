1a6fc375d584352d73ecf9ec22be1982ecfebdd7
OwPowell <owen@powellfam.net>
2023-02-01 10:43:10 -0500

    Added basic functionality to import data

---
+++ TremorDataAnalysis/tremorTracker.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import os
+
+def main():
+    print(import_data('mmc1.csv'))
+
+def import_data(filepath):
+    directory = os.path.dirname(os.path.abspath(__file__))
+    datafile = os.path.join(directory, filepath)
+    data = pd.read_csv(datafile, delim_whitespace=True)
+    return data
+
+if __name__ == '__main__':
+    main()
