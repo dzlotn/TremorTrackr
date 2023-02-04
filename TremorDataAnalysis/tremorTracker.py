f021778c9c4bd687e63a6e823fe168b1ebefabed
OwPowell <owen@powellfam.net>
2023-02-03 23:42:41 -0500

    Added headers to the csv data

--- TremorDataAnalysis/tremorTracker.py
+++ TremorDataAnalysis/tremorTracker.py
@@ -5,17 +5,18 @@ import matplotlib.pyplot as plt
 from scipy import signal
 
 
-
 def main():
     data = import_data('TestData\\mmc1.csv')
     data = data[:2000]
-    time = data.iloc[:,0]
-    acc = data.iloc[:,1]
-    emg = data.iloc[:,2]
-    
+    time = data['Time']
+    acc = data['RightACC']
+    emg = data['RightEMGext']
+
+    # Filter data
     acc_output = butter_filter(acc, 2, 30)
     emg_output = butter_filter(emg, 15, 250)
 
+    # Display data
     fig, axs = plt.subplots(2)
     axs[0].plot(time, acc, label='actual')
     axs[0].plot(time, acc_output, label='filtered')
@@ -26,10 +27,12 @@ def main():
     plt.show()
 
 
+''' Do a bandpass filter on data with low and high being the min and max frequencies'''
 def butter_filter(data, low, high):
     band_filter = signal.butter(10, [low, high], btype='bandpass', output='sos', fs=1000)
     return signal.sosfilt(band_filter, data)
 
+''' Import a space-separated csv file into a pandas dataFrame'''
 def import_data(filepath):
     directory = os.path.dirname(os.path.abspath(__file__))
     datafile = os.path.join(directory, filepath)
