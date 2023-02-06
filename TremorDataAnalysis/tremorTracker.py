6ef2dcf78bdd16e7666ce860a9971d531411fcb8
Dzlotnick <daniel.zlotnick5@gmail.com>
2023-02-06 12:09:09 -0500

    Did more work on the EMG data filtration

--- TremorDataAnalysis/tremorTracker.py
+++ TremorDataAnalysis/tremorTracker.py
@@ -7,14 +7,15 @@ from scipy import signal
 
 def main():
     data = import_data('TestData\\mmc1.csv')
-    data = data[:2000]
+    data = data[:1000]
     time = data['Time']
     acc = data['RightACC']
-    emg = data['RightEMGext']
+    emg = data['RightEMGflex']
 
     # Filter data
     acc_output = butter_filter(acc, 2, 30)
-    emg_output = butter_filter(emg, 15, 250)
+    emg_output = np.absolute(butter_filter(emg, 5,250))
+   
 
     # Display data
     fig, axs = plt.subplots(2)
@@ -25,6 +26,7 @@ def main():
     axs[1].plot(time, emg_output, label='filtered')
     axs[1].set_title('EMG')
     plt.show()
+    print(emg_output)
 
 
 ''' Do a bandpass filter on data with low and high being the min and max frequencies'''
@@ -32,7 +34,11 @@ def butter_filter(data, low, high):
     band_filter = signal.butter(10, [low, high], btype='bandpass', output='sos', fs=1000)
     return signal.sosfilt(band_filter, data)
 
+def ellip_filter(data, low):
+    band_filter = signal.ellip(2, 0.5, 40,low, btype='lowpass',  output='sos', fs=1000 )
+    return signal.sosfilt(band_filter, data)
 ''' Import a space-separated csv file into a pandas dataFrame'''
+    
 def import_data(filepath):
     directory = os.path.dirname(os.path.abspath(__file__))
     datafile = os.path.join(directory, filepath)
