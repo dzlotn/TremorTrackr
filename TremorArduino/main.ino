fc5d73d0d6120e4b79be62a34451189a623de92a
OwPowell <owen@powellfam.net>
2023-03-01 11:58:41 -0500

    Started arduino code

---
+++ TremorArduino/main.ino
@@ -0,0 +1,18 @@
+int EMGPin = A6;
+int IMUCSPin = D9;
+int IMUDOPin = D10;
+int IMUSDAPin = D11;
+int IMUSCLPin = D12;
+
+void setup() {
+  pinMode(EMGPin, INPUT);
+  pinMode(IMUCSPin, INPUT);
+  pinMode(IMUDOPin, INPUT);
+  pinMode(IMUSDAPin, INPUT);
+  pinMode(IMUSCLPin, INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
