949e5040d29024594a0513e2535f2c558477b0f7
OwPowell <owen@powellfam.net>
2023-03-03 14:08:18 -0500

    Fixed images with Flask

--- TremorWebsite/app.py
+++ TremorWebsite/app.py
@@ -6,7 +6,31 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html", title="Index")
+    return render_template("index.html")
+
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/interpretation")
+def interpretation():
+    return render_template("interpretation.html")
+
+@app.route("/procedure")
+def procedure():
+    return render_template("procedure.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/signIn")
+def signIn():
+    return render_template("signIn.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
