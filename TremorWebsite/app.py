4e57b614ed9c0c9a8bd1f34b824c22581a2a8813
OwPowell <owen@powellfam.net>
2023-03-13 12:33:59 -0400

    Added test function

--- TremorWebsite/app.py
+++ TremorWebsite/app.py
@@ -4,6 +4,9 @@ import pyrebase
 
 app = Flask(__name__)
 
+config = {}
+key = 0
+
 @app.route("/")
 def index():
     return render_template("index.html")
@@ -31,6 +34,58 @@ def register():
 @app.route("/signIn")
 def signIn():
     return render_template("signIn.html")
+
+@app.route("/test", methods=["GET", "POST"])
+def test():
+    global config, userID, db, timeStamp, key
+    
+    # POST request (FB configuration sent from login.js)
+    if request.method == "POST":
+        # Receive FB credentials, pop uid and assign to userID
+        config = request.get_json()
+        userID = config.pop("userID")
+
+        print("UserID: " + userID, flush = True)
+        print(config, flush = True)
+
+        # Initialize firebase connection
+        firebase = pyrebase.initialize_app(config)
+
+        # Create database object
+        db = firebase.database() # root node
+
+        # Timestamp
+        timeStamp = datetime.now().strftime("%d-%m-%Y %H:%M:%S")
+
+        # db.child('users/' + userID + '/data/' + timeStamp).update({'testKey2':'testValue'})
+
+        return 'Success', 200
+    
+    else:
+        # Code to GET data from Arduino will go here
+        if(bool(config) == False):
+            print("FB config is empty")
+
+        else:
+            # Take parameters from Arduino request & assign value to variable "value"
+
+            value = request.args.get('distance')
+
+            print("Distance: " + value, flush = True)
+
+            db.child('users/' + userID + '/data/' + timeStamp).update({key:value})
+
+            key += 1
+
+        return 'Success'
+    
+    # GET Request
+
+# Driver Function
+if __name__ == "__main__":
+
+    #Run app through port 5000 on 
+    app.run(debug=True, host='172.20.10.4', port=5000)
 
 if __name__ == '__main__':
     app.run(debug=True)
