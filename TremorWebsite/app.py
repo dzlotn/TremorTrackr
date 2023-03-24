efba2aa9444733cf4f79f175ecba98a3dc1f8a75
Dzlotnick <daniel.zlotnick5@gmail.com>
2023-03-23 22:41:14 -0400

    Revert "home page edits"
    This reverts commit 532ae5074a0dcc8a53eefd47073f723d6db11ff9.

--- TremorWebsite/app.py
+++ TremorWebsite/app.py
@@ -123,7 +123,7 @@ def test():
 
 def run_flask():
     #Run app through port 5000 on 
-    app.run(debug=True, host='192.168.50.218', port=5000)
+    app.run(debug=True, host='127.0.0.1', port=5000)
 
 
 if __name__ == '__main__':
