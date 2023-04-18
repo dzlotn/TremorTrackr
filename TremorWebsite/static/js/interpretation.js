7aee18823675633e7f8d0ecee04b58937efd4c77
Dzlotnick <daniel.zlotnick5@gmail.com>
2023-04-18 08:23:04 -0400

    navbar fixes, responsiveness upgrade, sensor diagram

--- TremorWebsite/static/js/interpretation.js
+++ TremorWebsite/static/js/interpretation.js
@@ -1,15 +1,43 @@
-//Sets the info box to different dataset infos depending on which diagram section the mouse is hovering over
+//Sets the info box to different dataset infos/names depending on which diagram section the mouse is hovering over. Also puts component information in the info box 
 //Deletes the info box div when not hovering over important component
 const interactiveSections = document.querySelectorAll('.interactive-section');
 const infoBox = document.querySelector('.info-box');
+const componentName = infoBox.querySelector('.component-name');
+const componentInfo = infoBox.querySelector('.component-info');
+const sensorInfo = {
+  'IMU Sensor': {
+    name: 'IMU Sensor',
+    info: 'The IMU (Inertial Measurement Unit) consists of a accelerometer that records both rotational and translational acceleration'
+  },
+  'Arduino Nano': {
+    name: 'Arduino Nano',
+    info: 'The Arduino Nano is a microcontroller board that is used to control and interact with the various sensors and components in the circuit.'
+  },
+  'Power Switches': {
+    name: 'Power Switches',
+    info: 'The power switches are used to turn the device on and off. They are connected to the battery and allow the user to control the power to the entire circuit.'
+  },
+  'EMG Sensor': {
+    name: 'EMG Sensor',
+    info: 'The EMG sensor measures the electrical activity of the muscles (voltage).'
+  },
+  'Battery': {
+    name: 'Battery',
+    info: 'The batteries provide power to the entire circuit. Shrink wrap is used to connect the button cell batteries in series.'
+  },
+  'Electrodes': {
+    name: 'Electrodes',
+    info: 'The electrodes are used to pick up the electrical signals from the muscles. They are placed on the skin and are connected to the EMG sensor.'
+  }
+};
 
 interactiveSections.forEach((section) => {
   section.addEventListener('mouseenter', () => {
-    const info = section.dataset.info;
-    infoBox.textContent = info;
+    const info = sensorInfo[section.dataset.info];
+    componentName.textContent = info.name;
+    componentInfo.textContent = info.info;
     infoBox.style.display = 'block';
     section.style.backgroundColor = 'rgba(255, 255, 255, 0.15)';
-
   });
 
   section.addEventListener('mouseleave', () => {
